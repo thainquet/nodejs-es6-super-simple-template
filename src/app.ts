93de727e73c86d8b56bf56f988762247019d4975
Thai Nguyen <thainq00@gmail.com>
2021-07-05 16:17:47 +0700

    add morgan

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
 import express from "express";
 import data from "./data";
+import path from "path";
+import morgan from "morgan";
+
+const srcDir = path.resolve(__dirname, "..");
 
 const app: express.Application = express();
 const port: number = 3000;
 
 app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
 
 app.get("/", (req, res) => {
+  res.sendFile(srcDir + "/html/index.html");
+});
+
+app.get("/data", (req, res) => {
   res.send(data[0]);
 });
 
